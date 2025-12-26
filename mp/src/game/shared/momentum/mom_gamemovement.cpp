1022283947416577cf48011cb54358f703efb2e5
chris <62446202+5xp@users.noreply.github.com>
2025-12-26 03:12:53 -0600

    fix(parkour): more correct crouch speed condition

--- mp/src/game/shared/momentum/mom_gamemovement.cpp
+++ mp/src/game/shared/momentum/mom_gamemovement.cpp
@@ -253,7 +253,7 @@ void CMomentumGameMovement::WalkMove()
         wishspeed = mv->m_flMaxSpeed;
     }
 
-    if (g_pGameModeSystem->GameModeIs(GAMEMODE_PARKOUR) && player->m_Local.m_bDucked)
+    if (g_pGameModeSystem->GameModeIs(GAMEMODE_PARKOUR) && ((mv->m_nButtons & IN_DUCK) || (player->GetFlags() & FL_DUCKING)))
     {
         wishspeed = PK_CROUCH_SPEED;
     }
